f6b9f13e675edebfc3973a7bbe9dc91ce359e0e3
Shubhra-dev <shuuvro.dev@gmail.com>
2023-10-17 23:33:19 +0600

    App LayOut Formatted and Data Fetching with React Router

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Cart from "./Features/cart/Cart";
 import CreateOrder from "./Features/order/CreateOrder";
 import Order from "./Features/order/Order";
-import Menu from "./Features/menu/Menu";
+import Menu, { menuLoader } from "./Features/menu/Menu";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
