7883880346278b68bac056408ecdda7659cf8235
Shubhra-dev <shuuvro.dev@gmail.com>
2023-10-17 23:06:46 +0600

    App Routing Completed with React Router V:6.4

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,5 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Cart from "./Features/cart/Cart";
+import CreateOrder from "./Features/order/CreateOrder";
+import Order from "./Features/order/Order";
+import Menu from "./Features/menu/Menu";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order?:orderId", element: <Order /> },
+    ],
+  },
+]);
 function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza</Link>
+      <p>Shuvro</p>
+    </header>
+  );
+}
+
+export default Header;
