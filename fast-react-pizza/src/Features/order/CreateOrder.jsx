3e8faf720bb9878cfd472aba8bb417cffae2c734
Shubhra-dev <shuuvro.dev@gmail.com>
2023-10-30 23:40:19 +0600

    Order form styling and order page styling added

--- fast-react-pizza/src/Features/order/CreateOrder.jsx
+++ fast-react-pizza/src/Features/order/CreateOrder.jsx
@@ -42,30 +42,39 @@ function CreateOrder() {
 
   return (
     <div className="px-4 py-6">
-      <h2>Ready to order? Let's go!</h2>
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
-          </div>
-          {formError?.phone && <p>{formError.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+            {formError?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formError.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex items-center gap-2">
           <input
             type="checkbox"
             name="priority"
@@ -74,7 +83,9 @@ function CreateOrder() {
             // onChange={(e) => setWithPriority(e.target.checked)}
             className="h-4 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
 const base =
-  'inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-40 disabled:cursor-not-allowed focus:ring-offset-2';
+  'inline-block text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-40 disabled:cursor-not-allowed focus:ring-offset-2';
 
 const styles = {
   primary: base + ' px-4 py-3 md:px-6 md:py-4',
   small: base + ' px-3 py-2 md:px-5 md:py-2.5 text-xs',
   secondary:
-    'px-4 py-2.5 md:px-6 md:py-3.5 inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-200 hover:bg-stone-200 border-2 border-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800  focus:outline-none focus:ring focus:ring-stone-300 focus:ring-opacity-40 disabled:cursor-not-allowed focus:ring-offset-2',
+    'px-4 text-sm py-2.5 md:px-6 md:py-3.5 inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-200 hover:bg-stone-200 border-2 border-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800  focus:outline-none focus:ring focus:ring-stone-300 focus:ring-opacity-40 disabled:cursor-not-allowed focus:ring-offset-2',
 };
 function Button({ children, to, disabled, type }) {
   console.log(to);
